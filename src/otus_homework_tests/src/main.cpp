b4e2431d6a26e34793facd7c9a12c49dba4c175a
winmord <winmord@yandex.ru>
2021-05-17 13:40:51 +0300

    added i_movable tests

--- src/otus_homework_tests/src/main.cpp
+++ src/otus_homework_tests/src/main.cpp
@@ -8,7 +8,7 @@
 using namespace fakeit;
 using namespace tank_battle_server;
 
-TEST_CASE("i_movable tests")
+TEST_CASE("i_movable moving test")
 {
 	movement_vector position({12, 5});
 
@@ -21,9 +21,62 @@ TEST_CASE("i_movable tests")
 		}
 	);
 
-	auto pointer_to_movable = std::shared_ptr<i_movable>(&i_movable_mock(), [](...){});
+	auto pointer_to_movable = std::shared_ptr<i_movable>(&i_movable_mock(), [](...)
+	{
+	});
+	
 	move_command cmd(pointer_to_movable);
 	cmd.execute();
 
 	REQUIRE(pointer_to_movable->get_position() == movement_vector({5, 8}));
+}
+
+TEST_CASE("i_movable position getting is not available test")
+{
+	Mock<i_movable> i_movable_mock;
+	When(Method(i_movable_mock, get_position)).AlwaysThrow(std::runtime_error("Position is not available"));
+	When(Method(i_movable_mock, get_velocity)).AlwaysReturn(movement_vector({0, 0}));
+
+	const auto pointer_to_movable = std::shared_ptr<i_movable>(&i_movable_mock(), [](...)
+	{
+	});
+	
+	move_command cmd(pointer_to_movable);
+
+	REQUIRE_THROWS_WITH(cmd.execute(), "Position is not available");
+}
+
+TEST_CASE("i_movable velocity getting is not available test")
+{
+	Mock<i_movable> i_movable_mock;
+	When(Method(i_movable_mock, get_position)).AlwaysReturn(movement_vector({0, 0}));
+	When(Method(i_movable_mock, get_velocity)).AlwaysThrow(std::runtime_error("Velocity is not available"));
+
+	const auto pointer_to_movable = std::shared_ptr<i_movable>(&i_movable_mock(), [](...)
+	{
+	});
+	
+	move_command cmd(pointer_to_movable);
+
+	REQUIRE_THROWS_WITH(cmd.execute(), "Velocity is not available");
+}
+
+TEST_CASE("i_movable position setting is not available test")
+{
+	Mock<i_movable> i_movable_mock;
+	When(Method(i_movable_mock, get_position)).AlwaysReturn(movement_vector({ 0, 0 }));
+	When(Method(i_movable_mock, get_velocity)).AlwaysReturn(movement_vector({ 0, 0 }));
+	When(Method(i_movable_mock, set_position)).AlwaysDo([&](movement_vector const& new_position)
+		{
+			throw std::runtime_error("Position setting is not available");
+		}
+	);
+
+	const auto pointer_to_movable = std::shared_ptr<i_movable>(&i_movable_mock(), [](...)
+	{
+	});
+
+	move_command cmd(pointer_to_movable);
+	
+	REQUIRE_THROWS_WITH(cmd.execute(), "Position setting is not available");
 }
